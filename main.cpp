42a1bddb4ab50ca6923798db07dc19dc07f22125
colesitzy <csitzberger@carthage.edu>
2017-10-06 14:31:49 -0500

    Changed base to float

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ void power(int base, int exponent);
 
 int main()
 {
-	power(5, 3);
-	power(5, 0)
+	power(5.1, 3);
+	power(5.5, 0)
 
 
 	system("pause");
 	return 0;
 }
 
-void power(int base, int exponent)
+void power(float base, int exponent)
 {
 	int result = base;
 	for (int i = 1; i < exponent; i++)
